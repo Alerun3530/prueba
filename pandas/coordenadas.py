ddf0809d1fe96fc7666c03ab09421e0b76995a77
Alerun3530 <corroa45@gmail.com>
2025-07-31 20:34:26 -0500

    first commit

---
+++ pandas/coordenadas.py
@@ -0,0 +1,26 @@
+# %%
+import pandas as pd
+from geopy.geocoders import Nominatim 
+# %%
+df = pd.read_csv('supermarkets.csv')
+df
+# %%
+nom = Nominatim(user_agent="my-application-name", timeout=10)
+# %%
+df['Address'] = df['Address'] + ', '+ df['City'] + ', '+ df['State'] + ', '+ df['Country'] 
+# %%
+df
+# %%
+df['Coordenadas'] = df['Address'].apply(nom.geocode)
+# %%
+df
+# %%
+df['Latitud'] = df['Coordenadas'].apply(lambda x: x.latitude if x != None else None)
+df 
+# %%
+df['Longitud'] = df['Coordenadas'].apply(lambda x: x.longitude if x != None else None)
+# %%
+df
+# %%
+nom.geocode('Carrera 7H #42-92, Barranquilla, Colombia')
+# %%
